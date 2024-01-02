b5cc8a8f7515794ad956bc389384a6425c8ded4b
ImagineBrkr <salvattore_25@hotmail.com>
2024-01-02 13:01:04 -0500

    Remove permisisons

--- api/start_backend.sh
+++ api/start_backend.sh
@@ -4,6 +4,9 @@ pip install -r requirements.txt
 
 LOG_FILE="./logs"
 python3 manage.py makemigrations
+python3 manage.py makemigrations ComprasApp
+python3 manage.py makemigrations PedidosApp
+python3 manage.py makemigrations CajaApp
 python3 manage.py migrate
 APP_CMD="nice -n-20 python3 manage.py runserver 0.0.0.0:8000 >> $LOG_FILE 2>&1 &"
 start_app() {
